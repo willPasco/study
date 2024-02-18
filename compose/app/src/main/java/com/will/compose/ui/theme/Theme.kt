db63968ea94d245f8270678986ae759cfe499df3
Willpasco <will.pasco.az@gmail.com>
2024-02-18 12:13:36 -0300

    Basic compose codelab.

--- compose/app/src/main/java/com/will/compose/ui/theme/Theme.kt
+++ compose/app/src/main/java/com/will/compose/ui/theme/Theme.kt
@@ -22,9 +22,10 @@ private val DarkColorScheme = darkColorScheme(
 )
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = LightBlue,
+    secondary = Navy,
+    tertiary = Blue,
+    surface = PurpleGrey40
 
     /* Other default colors to override
     background = Color(0xFFFFFBFE),
